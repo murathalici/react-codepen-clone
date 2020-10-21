80937d6eb600d514c7774fd37ef879eff0083729
Murat Halici <mhalici@collegeboard.org>
2020-10-20 23:02:30 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorate from '../hooks/useLocalStorage'
+
+function App() {
+  const [html, setHtml] = useLocalStorate('html', '');
+  const [css, setCss] = useLocalStorate('css','');
+  const [js, setJs] = useLocalStorate('js','');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc( `
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+    `)
+    }, 500)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor 
+          language="xml" 
+          displayName="HTML" 
+          value={html} 
+          onChange={setHtml} 
+        />
+        <Editor 
+          language="css" 
+          displayName="CSS" 
+          value={css} 
+          onChange={setCss}
+        />
+        <Editor 
+          language="javascript" 
+          displayName="JS" 
+          value={js} 
+          onChange={setJs} 
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          sr
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
